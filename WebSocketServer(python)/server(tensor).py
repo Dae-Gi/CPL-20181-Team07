f953e822da6336b41edb43a6c17eec437a76e68b
wjc167 <34672105+wjc167@users.noreply.github.com>
2018-05-17 00:52:39 +0900

    텐서플로우

---
+++ WebSocketServer(python)/server(tensor).py
@@ -0,0 +1,74 @@
+# -*- coding: cp949 -*- 
+import tensorflow as tf
+import numpy as np
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+import json
+
+xy = np.loadtxt('train.txt', unpack =True, dtype='float32')
+x_data = xy[:-1]
+x_data = np.transpose(x_data)
+
+thr_max = max(x_data[:, 0])
+lat_max = max(x_data[:, 1])
+
+x_data[:, 0] = x_data[:, 0] / thr_max
+x_data[:, 1] = x_data[:, 1] / lat_max
+
+y_data = np.reshape(xy[-1], (-1))
+print(x_data.shape, y_data.shape)
+
+X = tf.placeholder(tf.float32, [None, 2])
+Y = tf.placeholder(tf.uint8, [None])
+Y_onehot = tf.one_hot(Y, 10)
+W = tf.Variable(tf.random_normal([2, 10]))
+b = tf.Variable(tf.random_normal([1]))
+
+hypothesis = tf.nn.relu(tf.matmul(X, W) + b)
+predictions = tf.argmax(hypothesis, 1)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels = Y_onehot))
+rate = tf.Variable(0.01)
+optimizer = tf.train.AdamOptimizer(rate)
+train = optimizer.minimize(cost)
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(50001):
+   w, c, _ = sess.run([W, cost, train], feed_dict = {X: x_data, Y: y_data})
+   if step % 10000 == 0:
+      print(step, c)
+
+class SimpleEcho(WebSocket):
+    def handleMessage(self):
+        msg = json.loads(self.data)
+        print('recv', msg)
+        f = open("train.txt", 'a')
+        x1 = msg['throughput']
+        x2 = msg['latency']         
+        y = msg['value']
+        data = str(x1) + " " + str(x2) + " " + str(y) + str('\n')
+        f.write(data)
+        try:
+            p = sess.run([predictions], feed_dict = {X:[[min(1, msg['throughput']/thr_max), min(1, msg['latency']/lat_max)]]})
+        except Exception as e:
+            print(e)
+        print('AI가 결정한 품질 : ', p[0][0])
+        msg['quality'] = int(p[0][0])
+        msg['reason'] = 'random'
+        print('send', msg)
+        f.close()
+        self.sendMessage(json.dumps(msg))
+
+    def handleConnected(self):
+        print(self.address, 'connected')
+
+    def handleClose(self):
+        print(self.address, 'closed')
+
+server = SimpleWebSocketServer('192.168.253.129', 9000, SimpleEcho)
+server.serveforever()
+
+while(True):
+   thr, lat = [float(i) for i in input().split()]
+   print([thr / thr_max, lat / lat_max])
+   p = sess.run([predictions], feed_dict = {X: [[thr / thr_max, lat / lat_max]]})
+   print("AI : ", p[0][0])
